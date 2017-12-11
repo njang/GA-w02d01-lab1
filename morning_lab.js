e9086a1441af18e072fb317c3594d4f086237423
Neo Jang <neo.w.jang@gmail.com>
2017-12-11 13:50:42 -0600

    Complete extra activities: cat combinator

--- morning_lab.js
+++ morning_lab.js
@@ -23,15 +23,65 @@ const monster = {
    color: "greenish",
    type: "plasm or ghost or something"
 }
-
 // What would you write to access the name and console.log it?
 console.log(monster.name);
-
 // What would you write to change the type to 'creature' (without changing it inside the object)
 monster.type = 'creature';
-
 // What would you write to add a key to the object called age, and set the age to 6?
 monster.age = 6;
-
 // console.log the object to make sure type is creature, and age is 6
 console.log(monster);
+
+// Ogres
+const adventurer = {
+	name: "Titanius Anglesmith",
+	hitPoints: 100,
+	strength: 10
+}
+
+const ogre = {
+	hitPoints: 200,
+	strength: 15,
+	weakAgainst: "fire"
+}
+
+const battle = () => {
+	while (adventurer.hitPoints > 0 || ogre.hitPoints > 0) {
+		console.log();
+	}
+}
+
+
+// Extra stuff: Cat combinator
+// 1. Mama cat
+const cat1 = {
+	name: 'Misty',
+	breed: 'Persian',
+	age: 6
+}
+
+console.log(cat1.age);
+console.log(cat1.breed);
+
+// 2. Papa cat
+const cat2 = {
+	name: 'Rusty',
+	breed: 'Stray',
+	age: 5
+}
+
+// 3. Combine cats!
+const combineCats = (obj1, obj2) => {
+	let offspring = {
+		name: obj1.name + obj2.name,
+		age: 1,
+		breed: obj1.breed + "-" + obj2.breed
+	}
+	return offspring;
+}
+console.log(combineCats(cat1, cat2));
+console.log(combineCats({ name: "Craig", age: 20, breed: "unknown" }, { name: "Linda", age: 20, breed: "undefined" }));
+
+// 4. Cat brain bender
+console.log(combineCats(combineCats(cat1, cat2), combineCats(cat1, cat2)));
+
